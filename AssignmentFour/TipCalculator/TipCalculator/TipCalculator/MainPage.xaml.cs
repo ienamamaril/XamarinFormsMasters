639a8442c9fcff0052de5cf507baa0168c68f6d0
Siena Mamaril <sienam@magenic.com>
2018-07-25 02:00:00 +0800

    UPDATES for AssignmentFour - TipCalculator

--- AssignmentFour/TipCalculator/TipCalculator/TipCalculator/MainPage.xaml.cs
+++ AssignmentFour/TipCalculator/TipCalculator/TipCalculator/MainPage.xaml.cs
@@ -13,28 +13,35 @@ namespace TipCalculator
         public MainPage()
         {
             InitializeComponent();
+            billEntry.Text = "101"; //TINAMAD NA KASI AKO MAG INPUT =)
         }
-
-
 
         void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
         {
-
             tipPercentage.Text = String.Format("{0:F1}%", e.NewValue);
-            tipAmount.Text = "0.00";
+
+            if (!string.IsNullOrEmpty(billEntry.Text) && !string.IsNullOrEmpty(billEntry.Text))
+            {
+                double billAmountValue = Convert.ToDouble(billEntry.Text);
+                tipAmount.Text = String.Format(" {0:F2}", billAmountValue * (Convert.ToDouble(String.Format("{0:F1}", e.NewValue)) / 100));
+                double totalAmountValue = Convert.ToDouble(billAmountValue + (billAmountValue * (Convert.ToDouble(String.Format("{0:F1}", e.NewValue)) / 100)));
+                totalAmount.Text = String.Format(" {0:F2}", totalAmountValue);
+            }                
         }
 
         void OnBtnFifteenClicked(object sender, EventArgs args)
         {
             if (!string.IsNullOrEmpty(billEntry.Text) && !string.IsNullOrEmpty(billEntry.Text))
-            {
-
+            {               
+                tipSlider.Value = 15;
+                double billAmountValue = Convert.ToDouble(billEntry.Text);
+                tipAmount.Text = String.Format(" {0:F2}", billAmountValue * 0.15f);
+                double totalAmountValue = Convert.ToDouble(billAmountValue + (billAmountValue * 0.15f));
+                totalAmount.Text = String.Format(" {0:F2}", totalAmountValue);
             }
             else
             {
                 App.Current.MainPage.DisplayAlert("WARNING! ", "Bill amount is required.", "OK");
-
-
             }
         }
 
@@ -42,13 +49,15 @@ namespace TipCalculator
         {
             if (!string.IsNullOrEmpty(billEntry.Text) && !string.IsNullOrEmpty(billEntry.Text))
             {
-
+                tipSlider.Value = 20;
+                double billAmountValue = Convert.ToDouble(billEntry.Text);
+                tipAmount.Text = String.Format(" {0:F2}", (billAmountValue * 0.20f));
+                double totalAmountValue = Convert.ToDouble(billAmountValue + (billAmountValue * 0.20f));
+                totalAmount.Text = String.Format(" {0:F2}", (totalAmountValue));
             }
             else
             {
                 App.Current.MainPage.DisplayAlert("WARNING! ", "Bill amount is required.", "OK");
-
-
             }
         }
 
@@ -56,13 +65,14 @@ namespace TipCalculator
         {
             if (!string.IsNullOrEmpty(billEntry.Text) && !string.IsNullOrEmpty(billEntry.Text))
             {
-
+                double itip = Convert.ToDouble(tipAmount.Text);
+                tipAmount.Text = String.Format(" {0}", Math.Floor(itip));
+                double itotal = Convert.ToDouble(totalAmount.Text);
+                totalAmount.Text = String.Format(" {0}", Math.Floor(itotal));
             }
             else
             {
                 App.Current.MainPage.DisplayAlert("WARNING! ", "Bill amount is required.", "OK");
-
-
             }
         }
 
@@ -70,13 +80,14 @@ namespace TipCalculator
         {
             if (!string.IsNullOrEmpty(billEntry.Text) && !string.IsNullOrEmpty(billEntry.Text))
             {
-
+                double itip = Convert.ToDouble(tipAmount.Text);
+                tipAmount.Text = String.Format(" {0}", Math.Ceiling(itip));
+                double itotal = Convert.ToDouble(totalAmount.Text);
+                totalAmount.Text = String.Format(" {0}", Math.Ceiling(itotal));
             }
             else
             {
                 App.Current.MainPage.DisplayAlert("WARNING! ", "Bill amount is required.", "OK");
-
-
             }
         }
     }
