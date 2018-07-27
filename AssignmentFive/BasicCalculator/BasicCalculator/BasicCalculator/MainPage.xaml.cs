c0606007208c270782d83a9531b6bd6d40808e78
Siena Mamaril <sienam@magenic.com>
2018-07-27 14:53:52 +0800

    FINAL commit for the Basic Calculator

--- AssignmentFive/BasicCalculator/BasicCalculator/BasicCalculator/MainPage.xaml.cs
+++ AssignmentFive/BasicCalculator/BasicCalculator/BasicCalculator/MainPage.xaml.cs
@@ -1,17 +1,97 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace BasicCalculator
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+        int currentState = 1;        
+        double firstNumber, secondNumber;
+        string mathOperator;
+
+        public MainPage()
 		{
 			InitializeComponent();
-		}
-	}
+            OnClearButton(this, null);
+        }
+
+        void OnNumberButton(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string pressed = button.Text;
+
+            if (this.calulatedDisplay.Text == "0" || currentState < 0)
+            {
+                this.calulatedDisplay.Text = "";
+                if (currentState < 0)
+                    currentState *= -1;
+            }
+
+            this.calulatedDisplay.Text += pressed;
+
+            double number;
+            if (double.TryParse(this.calulatedDisplay.Text, out number))
+            {
+                this.calulatedDisplay.Text = number.ToString("N0");
+                if (currentState == 1)
+                {
+                    firstNumber = number;
+                }
+                else
+                {
+                    secondNumber = number;
+                }
+            }
+        }
+
+        void OnOperatorButton(object sender, EventArgs e)
+        {
+            currentState = -2;
+            Button button = (Button)sender;
+            string pressed = button.Text;
+            mathOperator = pressed;
+        }
+
+        void OnCalculateButton(object sender, EventArgs e)
+        {
+            if (currentState == 2)
+            {                
+                var result = CalculateAnswer(firstNumber, secondNumber, mathOperator);
+                this.calulatedDisplay.Text = CalculateAnswer(firstNumber, secondNumber, mathOperator).ToString("N0");
+                firstNumber = result;
+                currentState = -1;                
+            }
+        }
+
+        public static double CalculateAnswer(double value1, double value2, string mathOperator)
+        {
+            double result = 0;
+
+            switch (mathOperator)
+            {
+                case "÷":
+                    result = value1 / value2;
+                    break;
+                case "×":
+                    result = value1 * value2;
+                    break;
+                case "+":
+                    result = value1 + value2;
+                    break;
+                case "-":
+                    result = value1 - value2;
+                    break;
+            }
+
+            return result;
+        }
+
+        void OnClearButton(object sender, EventArgs e)
+        {
+            firstNumber = 0;
+            secondNumber = 0;
+            currentState = 1;
+            this.calulatedDisplay.Text = "0";
+        }
+    }
 }
--- AssignmentFive/BasicCalculator/BasicCalculator/BasicCalculator/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ AssignmentFive/BasicCalculator/BasicCalculator/BasicCalculator/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace BasicCalculator {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Label calulatedDisplay;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            calulatedDisplay = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "calulatedDisplay");
         }
     }
 }
